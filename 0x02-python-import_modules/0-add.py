3e82e49ac1caa0c81f065f44f697042a0e8fc434
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-03-09 21:52:29 +0300

    Add add function

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from add_0 import add
+
+a = 1
+b = 2
+
+def addition():
+    """Adds 1 and 2
+
+    Prints the result
+    """
+    sum = add(a, b)
+    print(f"{a} + {b} = {sum}")
+
+if (__name__ == "__main__"):
+    addition()
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,4 @@
+#Import & Modules
+
+This folder contains code written when learning about python modules and
+importing them
