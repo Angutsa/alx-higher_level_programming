1e857d50ebd9241f3c7cd694646eda4040a0d2d0
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-03-09 23:08:32 +0300

    Add function for task 05

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from variable_load_5 import a
+
+
+def print_variable():
+    """Print variable a from module
+
+    """
+
+    print(f"{a}")
+
+
+if (__name__ == '__main__'):
+    print_variable()
