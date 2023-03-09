6e3f1fc4537dce7a5d0c373c26924f3ecd722cd3
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-03-09 20:32:08 +0300

    Add program to check last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit_py.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+lastdigit = number % 10
+start = "Last digit of "
+
+if (lastdigit > 5):
+    print(f"{start}{number} is {lastdigit} and is greater than 5")
+elif (lastdigit == 0):
+    print(f"{start}{number} is {lastdigit} and is 0")
+elif (lastdigit < 6 and lastdigit != 0):
+    print(f"{start}{number} is {lastdigit} and is less than 6 and not 0")
