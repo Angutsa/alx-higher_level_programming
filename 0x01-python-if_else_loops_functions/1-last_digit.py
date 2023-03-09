bde89cb7a998162666ea46442559e1d382cbd5ec
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-03-09 21:04:57 +0300

    Correct programs

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastdigit = number % 10
+
+if (number < 0):
+    lastdigit = number % -10
+elif (number > 0):
+    lastdigit = number % 10
+
 start = "Last digit of "
 
 if (lastdigit > 5):
