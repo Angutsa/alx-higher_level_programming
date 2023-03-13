2a62ce23b98f4f7898e0b35aece588a8cb362007
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-03-13 20:35:34 +0300

    Add solution to task 0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def print_list_integer(my_list=[]):
+    """prints list of integers passed
+
+    """
+
+    for num in my_list:
+        print("{}".format(num))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,3 @@
+#Lists and Tuples
+
+This folder contains code written when learning about python tuples and lists
