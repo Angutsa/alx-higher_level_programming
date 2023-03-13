605f75b6b1d44ab9be50b555edd5800222ed64ea
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-03-13 21:05:32 +0300

    Add solution to task 0x03-03

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    """Prints list in reverse
+
+    my_list: list to be reversed
+    """
+
+    my_list.reverse()
+    for x in my_list:
+        print("{}".format(x))
