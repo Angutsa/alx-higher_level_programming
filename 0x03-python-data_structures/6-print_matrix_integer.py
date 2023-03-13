95473263d302b4586cd27b27e2a22ad0d45ff85b
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-03-13 21:58:14 +0300

    Add solution for task 0x03-06

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for x in row:
+            if (x == (row[len(row) - 1])):
+                print("{}".format(x), end="")
+            else:
+                print("{}".format(x), end=" ")
+        print("")
