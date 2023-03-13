7facf4ded518094bc0b52ec20175d962b02c0bfa
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-03-13 20:53:24 +0300

    Add solution to task 1

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,6 +4,9 @@
 def print_list_integer(my_list=[]):
     """prints list of integers passed
 
+
+    my_list: list of integers to be printed
+
     """
 
     for num in my_list:
---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """returns the element at requested index
+    my_list: list to be indexed
+    idx: index
+    """
+
+    if ((idx > 0) and (idx < len(my_list))):
+        return (my_list[idx])
