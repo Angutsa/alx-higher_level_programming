9baa048d5beb25fb8736456102cad6419bff8800
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-03-13 22:02:43 +0300

    Fix code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    """Prints an integer matrix
+    matrix: matrix to print
+    """
+
     for row in matrix:
         for x in row:
             if (x == (row[len(row) - 1])):
