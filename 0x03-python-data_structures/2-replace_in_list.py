2172b7b122e8aa0625afcf3787a120a3490a645d
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-03-14 20:12:58 +0300

    Fix code

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -7,7 +7,7 @@ def replace_in_list(my_list, idx, element):
     element: replacement value
     """
 
-    if ((idx > 0) and (idx < len(my_list))):
+    if ((idx >= 0) and (idx < len(my_list))):
         my_list[idx] = element
         return (my_list)
 
