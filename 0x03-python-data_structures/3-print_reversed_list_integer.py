5350790284abb97703c49c067f4ca2514be13ce0
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-03-14 20:16:20 +0300

    Fix code

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,7 +6,7 @@ def print_reversed_list_integer(my_list=[]):
     my_list: list to be reversed
     """
 
-    if (len(my_list) == 0):
+    if (my_list == None):
         return
 
     my_list.reverse()
