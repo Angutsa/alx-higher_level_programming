dc8412b2324568ee98498f7b7fc3199cda6a0489
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-03-14 20:22:06 +0300

    Fix code

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,11 +9,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if (len_a == 1):
         a1 = tuple_a[0]
     elif (len_a != 0):
-        a1, a2 = tuple_a
+        a1, a2 = tuple_a[0], tuple_a[1]
 
     if (len_b == 1):
         b1 = tuple_b[0]
     elif (len_b != 0):
-        b1, b2 = tuple_b
+        b1, b2 = tuple_b[0], tuple_b[1]
 
     return (a1+b1, a2+b2)
