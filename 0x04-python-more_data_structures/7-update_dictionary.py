88ad706e577d1d5a6acbd94e021f4e640ccf24cb
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-03-15 22:34:56 +0300

    Add solution for task 0x04-07

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """Adds or replaces a key/value in the dictionary"""
+
+    new_dict = a_dictionary
+    new_dict[key] = value
+    return (new_dict)
