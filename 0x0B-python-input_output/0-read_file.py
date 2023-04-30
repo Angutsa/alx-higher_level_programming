57e0b42a7920594af688bd3f0985a1d5e2eadfce
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-04-30 14:38:59 +0300

    Fix code

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""Contains a function to read from a file"""
+"""Contains a function to read from a file
+"""
 
 
 def read_file(filename=""):
@@ -12,5 +13,3 @@ def read_file(filename=""):
 
     with open(filename, mode="r", encoding="utf-8") as f:
         print(f.read())
-
-read_file("my_file_0.txt")
