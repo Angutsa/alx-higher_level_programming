61d9c4729de213309a94947fb2c2cb0793f5319e
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-06-05 22:11:27 +0300

    Add solutions

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Rectangle shape module
+
+This module creates and modifies a reactangle
+"""
+
+
+class Rectangle:
+    """Defines a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,4 @@
+# Python - More Classes & Objects
+
+This folder contains code written when delving deeper into Python classes and
+Objects
