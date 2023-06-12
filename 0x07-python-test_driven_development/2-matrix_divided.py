d5e95e09a77a1130008dc66a91c20ee2e35a2557
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-06-12 22:23:32 +0300

    Add solutions

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+"""Contains a divide matrix function
+
+"""
+
+
+def matrix_divided(matrix, div):
+    """divides all the elements of the matrix"""
+
+    # Validate input
+    not_matrix = "matrix must be a matrix (list of lists) of integers/floats"
+
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    if type(matrix) is not list:
+        raise TypeError(not_matrix)
+
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError(not_matrix)
+
+        for item in row:
+            if type(item) not in [int, float]:
+                raise TypeError(not_matrix)
+
+    row_length = len(matrix[0])
+    for row in matrix:
+        if (len(row)) != row_length:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    if div != float('inf') and div == div:
+        div = round(div, 2)
+
+    # Actual Division
+    matrix_div = []
+    index = 0
+    for row in matrix:
+        matrix_div.append([])
+        for item in row:
+            quotient = item / div
+            if quotient != float('inf') and quotient == quotient and quotient:
+                quotient = round(quotient, 2)
+
+            matrix_div[index].append(quotient)
+        index += 1
+
+    return matrix_div
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Module contains a function that adds a new line in text"""
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines after '.', '?' or ':'
+
+    Args:
+        text (str): text to be parsed
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    new_text = ""
+    for letter in text:
+        if letter in [".", "?", ":"]:
+            new_text = new_text + "{}\n\n".format(letter)
+        else:
+            new_text = new_text + letter
+
+        if new_text[-2:] == "\n ":
+            new_text = new_text[0:-1]
+
+    new_text = new_text.replace("\n\n ", "\n\n")
+
+    print(new_text, end="")
